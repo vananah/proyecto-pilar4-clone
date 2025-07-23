e0d3efab3faac4fc1da0a729cb47e689fba39d41
vananah <90639432+vananah@users.noreply.github.com>
2025-07-23 18:23:05 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import StreamingResponse
 import pandas as pd
 import io
 
-from utils import analizar_respuestas_p4
+from src.utils import analizar_respuestas_p4
 
 app = FastAPI()
 
